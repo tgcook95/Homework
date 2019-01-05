98c604254b9a682389ba3df816b9f7bd28949018
Taylor Cook <cooktaylor95@gmail.com>
2019-01-05 00:27:15 -0600

    Broke and fixed it!

--- Activity4b.py
+++ Activity4b.py
@@ -1,18 +1,23 @@
 import math
-import time
+
 
 def main():
     print(display_title())
-    print ("Base delivery for a 1 lb package is $7.95 for a 2 hour or less delivery.  Add "
-           "$2 for a 1 hour or less delivery. ")
+    print("Base delivery for a 1 lb package is $7.95 for a 2 hour or less delivery.  Add "
+          "$2 for a 1 hour or less delivery. ")
 
     valid = False
     while not valid:
         time = input("Enter 2 for 2 hour or less or a 1 for 1 hour or less: ")
         try:
             time = int(time)
-        except:
+        except ValueError:
             print("Please enter a numeric value.")
+            continue
+        except Exception as e:
+            print("Please enter a numeric value.")
+            continue
+
         if time == 1 or time == 2:
             valid = True
         else:
@@ -24,28 +29,40 @@ def main():
                        "starting at 2 lbs up to 50 lbs: ")
         try:
             weight = float(weight)
-        except:
+        except ValueError:
             print("Please enter a numeric value.")
+            continue
+        except Exception as e:
+            print("Please enter a numeric value.")
+            continue
+
         if weight > 50 or weight < 0:
             print("Please enter a weight between 0 and 50")
             print(" ")
-            ##Valid = False
         else:
             valid = True
 
-    print ("Total delivery price for package: " + get_delivery_price(weight, time))
-    print (" ")
-    var = input("Continue (y/n)? ")
+    print("Total delivery price for package: " + get_delivery_price(weight, time))
+    print(" ")
 
-    if var == "y":
-        main()
-    if var == "n":
-        print ("Bye")
+    valid = False
+    while not valid:
+        var = input("Continue (y/n)? ")
+        var = var.lower()
+
+        if var == "y":
+            main()
+        elif var == "n":
+            print("Bye")
+            valid = True
+        else:
+            continue
 
 
 def display_title():
-    title = "ISQA 4900 Metro Courier Service Delivery Calculator!"
+    title = "\nISQA 4900 Metro Courier Service Delivery Calculator!"
     return title
+
 
 def get_delivery_price(weight, time):
 
@@ -54,9 +71,10 @@ def get_delivery_price(weight, time):
     else:
         price = 7.95 + math.ceil(weight-2)
 
-    if time < 2:
+    if time == 1:
         price += 2.00
 
     return str(price)
+
 
 main()
